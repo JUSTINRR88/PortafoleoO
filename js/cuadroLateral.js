05a7b2cb0720b971702997a53d6154db28df37ca
JUSTINRR88 <126489355+JUSTINRR88@users.noreply.github.com>
2024-03-13 21:10:13 -0600

    Add files via upload

---
+++ js/cuadroLateral.js
@@ -0,0 +1,7 @@
+let cuadroLateral = `
+    <div class="info">
+        <div class="log">JB</div>
+        <div class="nombre">Bercian</div>
+    </div>
+`;
+export{cuadroLateral}
---
+++ js/data_web.js
@@ -0,0 +1,26 @@
+let webs = [
+    {
+        nombre: "Juego de Memoria",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio: "https://github.com/JUSTINRR88/Juego-de-Memoria",
+        githubpage: "https://justinrr88.github.io/Juego-de-Memoria/ ",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
+        nombre: "Maquetacion",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio: "https://github.com/JUSTINRR88/Login-Personal",
+        githubpage: "https://justinrr88.github.io/Login-Personal/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
+        nombre: "Habilidades",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio: "https://github.com/JUSTINRR88/TodoList_2024",
+        githubpage: "https://justinrr88.github.io/TodoList_2024/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    }
+
+];
+
+export {webs}
