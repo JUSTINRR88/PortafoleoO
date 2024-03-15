ced8bfd6c38727b554b452b82da68bc647b8ae7d
JUSTINRR88 <justinbercian1@gmail.com>
2024-03-15 10:48:34 -0600

    v1

--- js/data_web.js
+++ js/data_web.js
@@ -1,26 +1,25 @@
 let webs = [
     {
         nombre: "Juego de Memoria",
-        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        img :"https://img.freepik.com/foto-gratis/controlador-sujecion-manos-alto-angulo_23-2149829169.jpg ",
         repositorio: "https://github.com/JUSTINRR88/Juego-de-Memoria",
         githubpage: "https://justinrr88.github.io/Juego-de-Memoria/ ",
         descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
     },
     {
         nombre: "Maquetacion",
-        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        img :"https://web-assets.esetstatic.com/wls/2023/2023-10/social-login-sso-pros-cons.jpeg",
         repositorio: "https://github.com/JUSTINRR88/Login-Personal",
         githubpage: "https://justinrr88.github.io/Login-Personal/",
         descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
     },
     {
-        nombre: "Habilidades",
-        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        nombre: "TOdoList",
+        img :"https://tiempodenegocios.com/wp-content/uploads/2017/10/lista-de-tareas.jpg",
         repositorio: "https://github.com/JUSTINRR88/TodoList_2024",
         githubpage: "https://justinrr88.github.io/TodoList_2024/",
         descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
     }
-
 ];
 
 export {webs}
